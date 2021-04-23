737b96d440a9ef01d30a3b6b5a213e97ce363329
Lee Eun Deok <pleed0215@gmail.com>
2021-04-23 18:01:36 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/pleed0215/jobcrawler
+
+go 1.16
+
+require github.com/PuerkitoBio/goquery v1.6.1
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const baseUrl string = "https://kr.indeed.com/jobs"
+
+func main() {
+	jobUrl, err := getJobUrl("python", 2)
+	if(err == nil) {
+		fmt.Println(jobUrl)
+	} else {
+		fmt.Println(err)
+	}
+}
+
+func getJobUrl(job string, page int) (string, error) {
+	if(page > 0) {
+		return fmt.Sprintf("%s?q=%s&start=%d", baseUrl, job, (page-1)*10), nil
+	} else {
+		return "", errors.New("page must greater than 0")
+	}
+	
+}
+
+func getPages() int {
+	res, err := http.Get(baseUrl)
+	checkError(err)
+	checkCode(res.StatusCode)
+
+	doc, err := goquery.NewDocumentFromRead(res.Body)
+	fmt.Println(doc)
+	return 0
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func checkCode(code int) {
+	if code != 200 {
+		log.Fatalln("fail to connect")
+	}
+}
